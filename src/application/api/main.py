ed69c8414eed5191aa41108f814e1f4018bfbd6a
ragecodemode <nikitaloskutov18@gmail.com>
2024-10-01 17:40:25 +0300

    Include router

--- src/application/api/main.py
+++ src/application/api/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+from src.application.api.users.handlers import router as user_router
+
 
 def create_app():
-    return FastAPI(
+    app =  FastAPI(
         title='Integration with an external API',
         docs_url='/api/docs',
         description='Simple integration with an external API',
         debug=True,
     )
+    
+    app.include_routers(user_router, prefix='user')
+    
+    return app
